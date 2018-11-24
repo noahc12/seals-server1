8bf071c7063501fff0c3b5f3adaee596a1298368
noahc12 <40617454+noahc12@users.noreply.github.com>
2018-11-24 12:19:51 -0800

    first commit

--- server.js
+++ server.js
@@ -1,72 +1,71 @@
+require('dotenv').load();
+
+
+var mongoose = require('mongoose');
 var express = require('express');
 var bodyParser = require('body-parser');
 var middleWare = require('./_middleware/hash');
-var cors = require('cors')
-
+var cors = require('cors');
 
 var app = new express();
-app.use(cors())
-app.use(bodyParser.json());
 
-app.get('/home', function(req, res){
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log('connected to database')
+});
+
+// define models for mongoose
+var blogsSchema = new mongoose.Schema({
+    title: String,
+    content: String
+})
+
+var Blog = mongoose.model('Blog', blogsSchema);
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('', function (req, res) {
     res.send('ok');
 })
-app.get('/blog', function(req, res){
-    res.send('hi');
-})
-app.get('/taco', function(req, res){
-    res.send('burrito');
-})
-app.get('/chicken', function(req, res){
-    res.send('KFC');
-})
-app.get('/jumanji', function(req, res){
-    res.send('rob likes it');
-})
-app.get('/heyyy', function(req, res){
-    res.send('3 y');
-})
-app.get('/boi', function(req, res){
-    res.send('jolly');
-})
-app.get('/yum', function(req, res){
-    res.send('bootylicious');
-})
-app.post('/pikachu', function(req, res){
-    res.send([{
-        title: 'my first journal',
-        content: 'my first journal content',
-        now: new Date(),
-        
-      },
-      {
-        title: 'my second journal',
-        content: 'my second journal content',
-        now: new Date(),
-      },
-      {
-        title: 'my third journal',
-        content: 'my third journal content',
-        now: new Date(),
-      }]);
-})
 
-app.post('/test', middleWare.change, function(req, res){
+app.post('/addNewBlog', function (req, res) {
     var data = req.body;
 
-    data.email = 'pikachu45@gmail.com';
+    var newBlog = new Blog(data);
 
-    res.send(data);
+    newBlog.save(function (err, data) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send({ data: 'Data was saved!' });
+        }
+    });
 })
-// app.post('/charmander', middleWare.squirtle, function(req, res){
-//     var data = req.body;
 
-//     data.email = 'pikachu45@gmail.com';
+app.get('/getData', function (req, res) {
+    Blog.find(function (err, data) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send({ data: data });
+        }
+    })
+})
 
-//     res.send(data);
-// })
+app.post('/getJournal', function (req, res) {
+    var id = req.body.id;
+
+    Blog.findById(id, function (err, data) {
+        res.send({ data: data })
+    });
+})
 
 var port = 3000;
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('listening on port', port);
 })
